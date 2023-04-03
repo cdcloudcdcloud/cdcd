6792e9449e48ee59346a2018e2f60b49646e3257
cdcloudcdcloud <54095858+cdcloudcdcloud@users.noreply.github.com>
2023-04-03 17:12:26 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,6 +8,14 @@ const nextConfig = {
         destination: "https://api.openai.com/:slug*",
       },
       {
+        source: "/p/:slug*",
+        destination: "https://chat.openai.com/:slug*",
+      },
+      {
+        source: "/chat/:slug*",
+        destination: "https://chat.openai.com/chat:slug*",
+      },
+      {
         source: "/proxy-sse/:slug*",
         destination: "/api/proxy-sse?path=:slug*",
       },
